86215f143058dac8a9a63228aa2afad97b9f2c18
kdw940822 <kdw940822@naver.com>
2023-04-18 17:28:28 +0900

    kdwook이 home 컨트롤러 수정

--- spring_mvc_board_kdw/src/main/java/com/itwillbs/spring_mvc_board_kdw/HomeController.java
+++ spring_mvc_board_kdw/src/main/java/com/itwillbs/spring_mvc_board_kdw/HomeController.java
@@ -16,6 +16,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		// kdwook 브랜치에서 추가한 내용
 		return "index";
 	}
 	
